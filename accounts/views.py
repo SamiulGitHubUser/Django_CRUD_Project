3a6e299c0e98f0257416c0c26e511699dc2b9807
Samiul <samiulislam@gmail.com>
2020-10-26 16:18:34 +0900

    created urls.py and views.py to accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    #return HttpResponse('Home')
+    return render(request, 'accounts/dashboard.html')
+
+def products(request):
+    #return HttpResponse('Products')
+    return render(request, 'accounts/products.html')
+
+def customers(request):
+    #return HttpResponse('Customers')
+    return render(request, 'accounts/customers.html')
