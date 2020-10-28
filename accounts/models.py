8ee66879506034b82b5b9e6c040473b438cef2b0
Samiul <samiulislam@gmail.com>
2020-10-28 10:49:35 +0900

    Create DB and add customer_table, tag_table, product_table and order_table.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+#Customer Model
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+#Tag Model
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+#Product Model
+class Product(models.Model):
+    CATEGORY = (    ('Indoor', 'Indoor'),
+                    ('Outdoor', 'Outdoor')
+               )
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.CharField(max_length=200, null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+#Order Model
+class Order(models.Model):
+    STATUS = (  ('Pending', 'Pending'),
+                ('Out for Delivery', 'Out for Delivery'),
+                ('Delivered', 'Delivered')
+             )
+
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
