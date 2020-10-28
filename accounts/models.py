334b2087c44dcf8e4551f122f2f26bf74125b7ea
Samiul <samiulislam@gmail.com>
2020-10-28 16:37:49 +0900

    add demo query files

--- accounts/models.py
+++ accounts/models.py
@@ -31,6 +31,7 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
     tags = models.ManyToManyField(Tag)
 
     def __str__(self):
@@ -45,5 +46,6 @@ class Order(models.Model):
 
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
