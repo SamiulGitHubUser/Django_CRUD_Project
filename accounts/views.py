0ebcff40928e49c51e14963912ed5774e1c3358f
Samiul <samiulislam@gmail.com>
2020-10-30 20:28:30 +0900

    Delete operation is completed.

--- accounts/views.py
+++ accounts/views.py
@@ -59,3 +59,12 @@ def updateOrder(request, pk):
 
     context = {'form':form}
     return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+
+    context = {'order':order}
+    return render(request, 'accounts/delete.html', context)
