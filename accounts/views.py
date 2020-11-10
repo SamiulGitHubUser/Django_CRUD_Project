b230db891469542ae546e26dde778d6441330c21
Samiul <samiulislam@gmail.com>
2020-11-10 15:49:34 +0900

    Role based perssion

--- accounts/views.py
+++ accounts/views.py
@@ -4,47 +4,49 @@ from django.forms import inlineformset_factory
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 #from django.contrib.auth.forms import UserCreationForm
-
 from accounts.models import *
 from accounts.forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
+from .decorators import unauthenticated_user, allowed_users, admin_only
+
 
 # Create your views here.
 #Register View
+@unauthenticated_user
 def registerPage(request):
-    if request.user.is_authenticated: #this is very bad way to use is_authenticated
-        return redirect('home')
-    else:
-        form = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, "Account was created for "+ user)
-                return redirect('login')
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
 
-        context = {'form':form}
-        return  render(request, 'accounts/register.html', context)
+            group = Group.objects.get(name='customer')
+            user.groups.add(group)
+
+            messages.success(request, "Account was created for "+ username)
+            return redirect('login')
+
+    context = {'form':form}
+    return  render(request, 'accounts/register.html', context)
 
 #Login View
+@unauthenticated_user
 def loginPage(request):
-    if request.user.is_authenticated: #this is very bad way to use is_authenticated
-        return redirect('home')
-    else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(request, username=username, password=password)
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
 
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-            else:
-                messages.info(request, 'Username or Password is incorrect!')
-        context = {}
-        return  render(request, 'accounts/login.html', context)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect!')
+    context = {}
+    return  render(request, 'accounts/login.html', context)
 
 #logout View
 def logoutUser(request):
@@ -53,6 +55,7 @@ def logoutUser(request):
 
 #Home View
 @login_required(login_url='login')
+@admin_only
 def home(request):
     #return HttpResponse('Home')
     orders = Order.objects.all()
@@ -67,6 +70,11 @@ def home(request):
 
     return render(request, 'accounts/dashboard.html', context)
 
+
+#User View
+def userPage(request):
+    context = {}
+    return render(request, 'accounts/user.html', context)
 
 #Product View
 @login_required(login_url='login')
